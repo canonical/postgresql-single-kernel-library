04b0ce57c37b31b1c217e6d9f2af163bac4d8ae2
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2025-09-09 13:28:47 -0300

    Move snap user to a literal
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@canonical.com>

--- single_kernel_postgresql/config/literals.py
+++ single_kernel_postgresql/config/literals.py
@@ -16,5 +16,6 @@ BACKUP_USER = "backup"
 MONITORING_USER = "monitoring"
 REPLICATION_USER = "replication"
 REWIND_USER = "rewind"
+SNAP_USER = "_daemon_"
 USER = "operator"
 SYSTEM_USERS = [MONITORING_USER, REPLICATION_USER, REWIND_USER, USER]
--- single_kernel_postgresql/utils/filesystem.py
+++ single_kernel_postgresql/utils/filesystem.py
@@ -5,14 +5,16 @@
 import os
 import pwd
 
+from ..config.literals import SNAP_USER
+
 
 def change_owner(path: str) -> None:
-    """Change the ownership of a file or a directory to the _daemon_ user.
+    """Change the ownership of a file or a directory to the snap user.
 
     Args:
         path: path to a file or directory.
     """
-    # Get the uid/gid for the _daemon_ user.
-    user_database = pwd.getpwnam("_daemon_")
+    # Get the uid/gid for the snap user.
+    user_database = pwd.getpwnam(SNAP_USER)
     # Set the correct ownership for the file or directory.
     os.chown(path, uid=user_database.pw_uid, gid=user_database.pw_gid)
