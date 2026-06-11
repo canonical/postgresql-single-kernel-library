e08b37b506306faa97d614f6c1ab2bda3f3229e9
Dragomir Penev <dragomir.penev@canonical.com>
2026-06-11 17:00:03 +0300

    Port of VM Patroni helper

--- single_kernel_postgresql/config/literals.py
+++ single_kernel_postgresql/config/literals.py
@@ -29,4 +29,44 @@ DIR_PERMISSIONS_READONLY = 0o750
 # Container name for K8s deployments
 CONTAINER_NAME = "postgresql"
 
+# Patroni constants
 API_REQUEST_TIMEOUT = 5
+PATRONI_CLUSTER_STATUS_ENDPOINT = "cluster"
+PATRONI_SERVICE_NAME = "snap.charmed-postgresql.patroni.service"
+PATRONI_SERVICE_DEFAULT_PATH = f"/etc/systemd/system/{PATRONI_SERVICE_NAME}"
+
+# TLS constants
+TLS_CA_BUNDLE_FILE = "peer_ca_bundle.pem"
+
+# Raft constants
+RAFT_PORT = 2222
+RAFT_PARTNER_PREFIX = "partner_node_status_server_"
+
+# Snap constants
+SNAP_COMMON_PATH = "/var/snap/charmed-postgresql/common"
+SNAP_CURRENT_PATH = "/var/snap/charmed-postgresql/current"
+DATA_DIR_SUBFOLDER = "16/main"
+
+SNAP_CONF_PATH = f"{SNAP_CURRENT_PATH}/etc"
+SNAP_DATA_PATH = f"{SNAP_COMMON_PATH}/var/lib"
+SNAP_LOGS_PATH = f"{SNAP_COMMON_PATH}/var/log"
+ARCHIVE_STORAGE_PATH = f"{SNAP_COMMON_PATH}/data/archive"
+LOGS_STORAGE_PATH = f"{SNAP_COMMON_PATH}/data/logs"
+TEMP_STORAGE_PATH = f"{SNAP_COMMON_PATH}/data/temp"
+
+PATRONI_CONF_PATH = f"{SNAP_CONF_PATH}/patroni"
+PATRONI_LOGS_PATH = f"{SNAP_LOGS_PATH}/patroni"
+
+PGBACKREST_CONF_PATH = f"{SNAP_CONF_PATH}/pgbackrest"
+PGBACKREST_LOGS_PATH = f"{SNAP_LOGS_PATH}/pgbackrest"
+
+POSTGRESQL_CONF_PATH = f"{SNAP_CONF_PATH}/postgresql"
+POSTGRESQL_DATA_PATH = f"{SNAP_DATA_PATH}/postgresql"
+POSTGRESQL_DATA_DIR = f"{POSTGRESQL_DATA_PATH}/{DATA_DIR_SUBFOLDER}"
+ARCHIVE_DATA_DIR = f"{ARCHIVE_STORAGE_PATH}/{DATA_DIR_SUBFOLDER}"
+LOGS_DATA_DIR = f"{LOGS_STORAGE_PATH}/{DATA_DIR_SUBFOLDER}"
+TEMP_DATA_DIR = f"{TEMP_STORAGE_PATH}/{DATA_DIR_SUBFOLDER}"
+POSTGRESQL_LOGS_PATH = f"{SNAP_LOGS_PATH}/postgresql"
+
+# Pgbackrest constants
+PGBACKREST_CONFIGURATION_FILE = f"--config={PGBACKREST_CONF_PATH}/pgbackrest.conf"
