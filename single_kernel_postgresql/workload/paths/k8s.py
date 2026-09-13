1f0411f9392c8f55cc4e76d310d8ab37fd6db0db
Marcelo Henrique Neppel <marcelo.neppel@canonical.com>
2026-09-13 11:18:06 -0300

    test(backups): S3 connection info view and S3 client coverage (5/19)
    Signed-off-by: Marcelo Henrique Neppel <marcelo.neppel@canonical.com>

--- single_kernel_postgresql/workload/paths/k8s.py
+++ single_kernel_postgresql/workload/paths/k8s.py
@@ -6,7 +6,6 @@ from charmlibs.pathops import PathProtocol
 
 from single_kernel_postgresql.config.literals import (
     K8S_DATA_PATH,
-    K8S_PGBACKREST_LOGS_PATH,
     PATRONI_LOGS_PATH,
     PGBACKREST_CONF_PATH,
     POSTGRESQL_CONF_FILE,
@@ -83,11 +82,6 @@ class K8sPaths(Paths):
     def patroni_logs(self) -> PathProtocol:
         """Path to the patroni logs."""
         return self.logs / PATRONI_LOGS_PATH
-
-    @property
-    def pgbackrest_logs(self) -> PathProtocol:
-        """Path to the pgbackrest logs."""
-        return self.logs / K8S_PGBACKREST_LOGS_PATH
 
     @property
     def pgbackrest_conf(self) -> PathProtocol:
